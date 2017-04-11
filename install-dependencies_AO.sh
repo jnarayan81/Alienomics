d7356644db67927e03fd4c73cf7b1733b5a27d9e
urbe <urbe@urbo214b>
2017-04-11 14:45:50 +0200

    updated BUSCO

--- install-dependencies_AO.sh
+++ install-dependencies_AO.sh
@@ -111,3 +111,69 @@ else
    echo "bwa.kit exists"
 fi
 
+
+: '
+Use database of your interest
+For more detail visit file:///home/urbe/Downloads/BUSCO_v2.0_userguide.pdf
+# Bacteria
+wget http://busco.ezlab.org/v2/datasets/bacteria_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/proteobacteria_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/rhizobiales_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/betaproteobacteria_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/gammaproteobacteria_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/enterobacteriales_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/deltaepsilonsub_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/actinobacteria_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/cyanobacteria_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/firmicutes_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/clostridia_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/lactobacillales_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/bacillales_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/bacteroidetes_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/spirochaetes_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/tenericutes_odb9.tar.gz
+        
+# Eukaryota
+wget http://busco.ezlab.org/v2/datasets/eukaryota_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/fungi_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/microsporidia_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/dikarya_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/ascomycota_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/pezizomycotina_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/eurotiomycetes_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/sordariomyceta_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/saccharomyceta_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/saccharomycetales_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/basidiomycota_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/metazoa_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/nematoda_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/arthropoda_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/insecta_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/endopterygota_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/hymenoptera_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/diptera_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/vertebrata_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/actinopterygii_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/tetrapoda_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/aves_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/mammalia_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/euarchontoglires_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/laurasiatheria_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/embryophyta_odb9.tar.gz
+wget http://busco.ezlab.org/v2/datasets/protists_ensembl.tar.gz
+wget http://busco.ezlab.org/v2/datasets/alveolata_stramenophiles_ensembl.tar.gz
+
+'
+
+if [ ! -d "$PWD/BUSCOData" ]; then
+    if [ ! -d "$PWD/BUSCOData" ]; then
+        mkdir $PWD/BUSCOData
+    fi
+    cd $PWD/BUSCOData
+    wget http://busco.ezlab.org/v2/datasets/metazoa_odb9.tar.gz
+    tar xzfp metazoa_odb9.tar.gz
+    #for a in *.fasta.gz; do gzip $a; done
+else
+   echo "BUSCOData database exists"
+fi
+
